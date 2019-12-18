9ef9aef7150c608b68f1de4da06353e558c46c8a
John Titus <john.titus@rackspace.com>
2019-12-18 16:38:08 -0600

    Sort outputs in alphabetic order

--- outputs.tf
+++ outputs.tf
@@ -1,48 +1,3 @@
-output "public_rdp_security_group_id" {
-  description = "Public RDP Security Group ID"
-  value       = aws_security_group.public_rdp_security_group.id
-}
-
-output "private_rdp_security_group_id" {
-  description = "Private RDP Security Group ID"
-  value       = aws_security_group.private_rdp_security_group.id
-}
-
-output "elastic_cache_memcache_security_group_id" {
-  description = "ElasticCache Memcache Security Group ID"
-  value       = aws_security_group.elastic_cache_memcache_security_group.id
-}
-
-output "elastic_cache_redis_security_group_id" {
-  description = "ElasticCache Redis Security Group ID"
-  value       = aws_security_group.elastic_cache_redis_security_group.id
-}
-
-output "public_ssh_security_group_id" {
-  description = "Public SSH Security Group ID"
-  value       = aws_security_group.public_ssh_security_group.id
-}
-
-output "private_ssh_security_group_id" {
-  description = "Private SSH Security Group ID"
-  value       = aws_security_group.private_ssh_security_group.id
-}
-
-output "public_web_security_group_id" {
-  description = "Public Web Security Group ID"
-  value       = aws_security_group.public_web_security_group.id
-}
-
-output "private_web_security_group_id" {
-  description = "Private Web Security Group ID"
-  value       = aws_security_group.private_web_security_group.id
-}
-
-output "nfs_security_group_id" {
-  description = "NFS Security Group ID"
-  value       = aws_security_group.nfs_security_group.id
-}
-
 output "efs_security_group_id" {
   description = "EFS Security Group ID"
   value       = aws_security_group.efs_security_group.id
@@ -58,6 +13,16 @@ output "eks_worker_security_group_id" {
   value       = aws_security_group.eks_worker_security_group.id
 }
 
+output "elastic_cache_memcache_security_group_id" {
+  description = "ElasticCache Memcache Security Group ID"
+  value       = aws_security_group.elastic_cache_memcache_security_group.id
+}
+
+output "elastic_cache_redis_security_group_id" {
+  description = "ElasticCache Redis Security Group ID"
+  value       = aws_security_group.elastic_cache_redis_security_group.id
+}
+
 output "mssql_security_group_id" {
   description = "MS SQL DB Security Group ID"
   value       = aws_security_group.mssql_security_group.id
@@ -66,6 +31,11 @@ output "mssql_security_group_id" {
 output "mysql_security_group_id" {
   description = "MySQL DB Security Group ID"
   value       = aws_security_group.mysql_security_group.id
+}
+
+output "nfs_security_group_id" {
+  description = "NFS Security Group ID"
+  value       = aws_security_group.nfs_security_group.id
 }
 
 output "oracle_security_group_id" {
@@ -78,14 +48,44 @@ output "postgres_security_group_id" {
   value       = aws_security_group.postgres_security_group.id
 }
 
-output "redshift_security_group_id" {
-  description = "Redshift Security Group ID"
-  value       = aws_security_group.redshift_security_group.id
-}
-
 output "private_ecs_security_group_id" {
   description = "Private ECS Security Group ID"
   value       = aws_security_group.private_ecs_security_group.id
+}
+
+output "private_rdp_security_group_id" {
+  description = "Private RDP Security Group ID"
+  value       = aws_security_group.private_rdp_security_group.id
+}
+
+output "private_ssh_security_group_id" {
+  description = "Private SSH Security Group ID"
+  value       = aws_security_group.private_ssh_security_group.id
+}
+
+output "private_web_security_group_id" {
+  description = "Private Web Security Group ID"
+  value       = aws_security_group.private_web_security_group.id
+}
+
+output "public_rdp_security_group_id" {
+  description = "Public RDP Security Group ID"
+  value       = aws_security_group.public_rdp_security_group.id
+}
+
+output "public_ssh_security_group_id" {
+  description = "Public SSH Security Group ID"
+  value       = aws_security_group.public_ssh_security_group.id
+}
+
+output "public_web_security_group_id" {
+  description = "Public Web Security Group ID"
+  value       = aws_security_group.public_web_security_group.id
+}
+
+output "redshift_security_group_id" {
+  description = "Redshift Security Group ID"
+  value       = aws_security_group.redshift_security_group.id
 }
 
 output "vpc_endpoint_security_group_id" {
